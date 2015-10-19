d2f097dcb6e3381cce8a9e5e01d9b8bcb7f01d37
Dylan Richardson <dylanrichardson47@gmail.com>
2015-10-19 18:31:39 -0400

    Add c++ PolCharacter and PolCharacterMovementComponent subclasses. Add functionality to support three sequential jumps.

---
+++ Source/Pol/PolCharacterMovementComponent.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/CharacterMovementComponent.h"
+#include "PolCharacterMovementComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UPolCharacterMovementComponent : public UCharacterMovementComponent
+{
+	GENERATED_UCLASS_BODY()
+
+public:
+
+	enum JumpState
+	{
+		First,
+		Second,
+		Third
+	};
+	
+	UPROPERTY(Category = "Character Movement: Jumping / Falling", EditAnywhere, BlueprintReadWrite, meta = (DisplayName = "Jump 1 Z Velocity", ClampMin = "0", UIMin = "0"))
+	float Jump1ZVelocity;
+
+	UPROPERTY(Category = "Character Movement: Jumping / Falling", EditAnywhere, BlueprintReadWrite, meta = (DisplayName = "Jump 2 Z Velocity", ClampMin = "0", UIMin = "0"))
+	float Jump2ZVelocity;
+
+	UPROPERTY(Category = "Character Movement: Jumping / Falling", EditAnywhere, BlueprintReadWrite, meta = (DisplayName = "Jump 3 Z Velocity", ClampMin = "0", UIMin = "0"))
+	float Jump3ZVelocity;
+
+	virtual bool DoJump(bool bReplayingMoves) override;
+
+	void setCurrJumpState(JumpState state);
+
+private:
+
+	JumpState currJumpState;
+	
+};
